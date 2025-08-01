b392d8f839a0d09f875282ac47febb5b19414357
Pranav Dumbre <pranavdumbre1911@gmail.com>
2025-08-01 16:56:52 +0530

    basic variable declaration

---
+++ variables.js
@@ -0,0 +1,14 @@
+var smsSendingLimit =200;
+var hasPermission = true;
+var username = "Pranav";
+var nothing = undefined;
+
+// don't touch below this line
+console.log("smsSendingLimit is a " + typeof smsSendingLimit);
+console.log("hasPermission is a " + typeof hasPermission);
+console.log("username is a " + typeof username);
+console.log("nothing is a " + typeof nothing);
+// smsSendingLimit is a number
+// hasPermission is a boolean
+// username is a string
+// nothing is a undefined
---
+++ variabless2.js
@@ -0,0 +1,32 @@
+const mySkillIssues1 = 42;
+mySkillIssues1 = 43; // TypeError: Assignment to constant variable.
+
+if (true) {
+  let mySkillIssues = 42;
+  const myConstSkillIssues = 43;
+}
+console.log(mySkillIssues); // ReferenceError: mySkillIssues is not defined
+console.log(myConstSkillIssues); // ReferenceError: myConstSkillIssues is not defined
+
+// mySkillIssues isn't scoped to its block (the if body), as most developers would expect.
+// Instead, it's scoped to the function that contains it (or the global scope if it's not in a function).
+// The let and const keywords are block-scoped:
+
+var messageText = "Welcome to Textio!";
+var isBirthday = true;
+
+if (isBirthday) {
+  const messageText = "Happy Birthday!";
+
+  console.log("sending birthday message..."); // sending birthday message...
+
+  console.log("messageText: ", messageText); // messageText:  Happy Birthday!
+
+}
+
+// don't touch below this line
+
+console.log("sending welcome message...");// sending welcome message...
+
+console.log("messageText: ", messageText);// messageText:  Welcome to Textio!
+
