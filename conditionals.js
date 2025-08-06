8c62a8be785871dcf7dc7eeea0343bed4f90758c
Pranav Dumbre <pranavdumbre1911@gmail.com>
2025-08-06 19:07:49 +0530

    conditional statements

---
+++ conditionals.js
@@ -0,0 +1,34 @@
+if (height > 6) {
+  console.log("You are super tall!");
+} else if (height > 4) {
+  console.log("You are tall enough!");
+} else {
+  console.log("You are not tall enough!");
+}
+
+
+let messageLen = 10;
+let maxMessageLen = 20;
+console.log(
+  "Trying to send a message of length:",
+  messageLen,
+  "and a max length of:",
+  maxMessageLen,
+);
+
+// don't touch above this line
+
+if (messageLen < maxMessageLen) {
+  console.log("Message sent");
+} else {
+  console.log("Message not sent");
+}
+/*
+comparison operators :
+=== equal to
+!== not equal to
+< less than
+> greater than
+<= less than or equal to
+>= greater than or equal to
+*/
